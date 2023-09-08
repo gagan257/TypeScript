cdb13cb35bd43dc9c68d629cc77123c0b6ad4665
Gagan Arora <arorag2577@gmail.com>
2023-09-08 11:13:52 +0530

    types of variables and usage

--- compiled-files/index.js
+++ compiled-files/index.js
@@ -1,3 +1,6 @@
 "use strict";
-let age = 20;
+let sales = 1234;
+let course = "TypeScript";
+let published = true;
+let level;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-let age: number = 20; // TS file with type defined
+let sales: number = 1234; // we can remove type define as TS will detect it automatically
+let course: string = "TypeScript";
+let published: boolean = true;
+let level; // TS will make it type any by default
 // HERE ARE SOME COMMENTS WHICH WILL NOT GET EMITTED IN ../compiled-files/index.js
