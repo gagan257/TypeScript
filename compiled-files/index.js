61a349ef86ba93e44c3d9bfbe7b1b6d1cac7fc67
Gagan Arora <arorag2577@gmail.com>
2023-09-19 11:51:08 +0530

    enums & functions

--- compiled-files/index.js
+++ compiled-files/index.js
@@ -7,4 +7,12 @@ let number = [1, 2, 3];
 let number2 = [];
 let number3 = [1, 2, 3];
 let user = [1, "Sample_Name"];
+var Size;
+(function (Size) {
+    Size[Size["small"] = 5] = "small";
+    Size[Size["medium"] = 6] = "medium";
+    Size[Size["large"] = 7] = "large";
+})(Size || (Size = {}));
+let mysize = Size.medium;
+console.log(mysize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { isContext } from "vm";
+
 let sales: number = 1234; // we can remove type define as TS will detect it automatically
 let course: string = "TypeScript";
 let published: boolean = true;
@@ -10,3 +12,28 @@ let number2 = []; // default type 'any[]'
 let number3 = [1, 2, 3]; // default type 'number[]' as we have values in array
 
 let user: [number, string] = [1, "Sample_Name"]; // define each position to use mixed types
+
+enum Size {
+  small = 5,
+  medium,
+  large,
+} // compiler sets default values starts from 1 and adding +1 for each index
+// if we set small = 5 now medium and large will auto become 6 & 7
+// auto only work with numbers
+let mysize: Size = Size.medium;
+console.log(mysize); // will get value of Size.medium
+
+function calcTax(income: number) {
+  // return type set to number(auto)
+  return income;
+}
+function calcTax2(income: number, year?: number): number {
+  // defining return type to avoid mistakes
+  if (income < 50000) {
+    return income * 1.2;
+  }
+  return income * 1.3;
+}
+
+calcTax2(10000); // here 2nd parameter year is optional (using ? in argc)
+// we can give default values in argc example: year = 2020 in line 30
