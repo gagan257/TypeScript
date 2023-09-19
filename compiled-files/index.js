fa70d17a2f2640adc15a5abf89d250074043cafc
Gagan Arora <arorag2577@gmail.com>
2023-09-19 11:11:36 +0530

    arrays and tuples

--- compiled-files/index.js
+++ compiled-files/index.js
@@ -3,4 +3,8 @@ let sales = 1234;
 let course = "TypeScript";
 let published = true;
 let level;
+let number = [1, 2, 3];
+let number2 = [];
+let number3 = [1, 2, 3];
+let user = [1, "Sample_Name"];
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,10 @@ let course: string = "TypeScript";
 let published: boolean = true;
 let level; // TS will make it type any by default
 // HERE ARE SOME COMMENTS WHICH WILL NOT GET EMITTED IN ../compiled-files/index.js
+
+let number: number[] = [1, 2, 3];
+// we cannot use [1,2,'3'] like JS
+let number2 = []; // default type 'any[]'
+let number3 = [1, 2, 3]; // default type 'number[]' as we have values in array
+
+let user: [number, string] = [1, "Sample_Name"]; // define each position to use mixed types
